8d56627b53a0bea27318df92f88c82cdcd1e6779
Savannah O. Kadima <savco2000@gmail.com>
2016-12-29 22:10:04 -0500

    Adding IArticle interface and refactoring ArticleListComponent to use it.

--- src/app/articles/article-list.component.ts
+++ src/app/articles/article-list.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core'
+import { IArticle } from './article'
 
 @Component({
-    selector:'ieindex-articles',
+    selector: 'ieindex-articles',
     templateUrl: 'app/articles/article-list.component.html'
 })
 
 export class ArticleListComponent {
-
+    articles: IArticle[] = [{
+        "id": 1,
+        "title": "All-One Comprehensive Immigration Reform",
+        "page": 12,
+        "issue": "Fall",
+        "publicationYear": 2010,
+        "isSupplement": "No",
+        "hyperlink": "/downloads/ie_julaug13_frontlines.pdf"
+    },
+    {
+        "id": 2,
+        "title": "Agent Provocative",
+        "page": 7,
+        "issue": "Summer",
+        "publicationYear": 2013,
+        "isSupplement": "Yes",
+        "hyperlink": "/downloads/ie_julaug13_frontlines.pdf"
+    },
+    {
+        "id": 3,
+        "title": "Service Learning in Tanzania",
+        "page": 19,
+        "issue": "Winter",
+        "publicationYear": 2015,
+        "isSupplement": "No",
+        "hyperlink": "/downloads/ie_julaug13_frontlines.pdf"
+    }];
 }
---
+++ src/app/articles/article.ts
@@ -0,0 +1,9 @@
+export interface IArticle {
+    id: number,
+    title: string,
+    page: number,
+    issue: string,
+    publicationYear: number,
+    isSupplement: string,
+    hyperlink: string
+}
